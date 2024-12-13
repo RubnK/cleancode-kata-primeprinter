ff4a67d8d8c5e651b726d251a794040f18099518
RubnK <40402492+RubnK@users.noreply.github.com>
2024-12-13 09:55:32 +0100

    Activity 6

---
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
@@ -0,0 +1,5 @@
+package com.cleancode.knuth;
+
+public class NumberPrinter {
+
+}
--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -2,9 +2,11 @@ package com.cleancode.knuth;
 
 public class PrimePrinter {
     private static final int numberOfPrimes = 1000;
+
     public static void main(String[] args) {
         PrimePrinterHelper helper = new PrimePrinterHelper(numberOfPrimes);
-        int[] primes = helper.print();
-        helper.printNumbers(primes, numberOfPrimes);
+        int[] primes = helper.generatePrimes();
+        NumberPrinter printer = new NumberPrinter(50, 4);
+        printer.printNumbers(primes, numberOfPrimes);
     }
 }
